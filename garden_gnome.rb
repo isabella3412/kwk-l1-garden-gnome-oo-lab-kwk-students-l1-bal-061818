e96f3a6731f521d14360ec78cc545f1fab2fdfbe
isabella3412 <izzybella17@icloud.com>
2018-06-22 20:07:19 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,25 @@
 # Code your instances here
+# Code your instances here
+class GardenGnome
+  atter-accessor :name :age :gluten_allergy :hat_color
+
+  def initailize(personality, hat_color ="red")
+    @personality = "evil"
+  end
+  def personality = (personality)
+    @personality = "evil"
+  end
+
+green = GardenGnome.new
+
+
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+
+  def shout
+    return "GNARLY!!!"
+  end
+
+  def introduce_self
+    "Hello humans, my name is Hubert, I am 267 years old, and you'll rue the day you crossed me!"
